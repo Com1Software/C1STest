6e3d84b13842ac8c465c8485ef53f29d35e8b2bd
Com1 Software, Inc <information@com1software.com>
2023-01-03 17:08:11 -0500

    Update main.go

--- main.go
+++ main.go
@@ -1,19 +1,20 @@
 package main
 
 import (
-	"github.com/Com1Software/C1S"
 	"fmt"
 	"os"
 	"runtime"
+
+	c1s "github.com/Com1Software/C1S"
 	term "github.com/nsf/termbox-go"
 )
 
 func main() {
+	cmdctl := c1s.CommandControl{}
 	fmt.Printf("C1Script\n")
 	fmt.Printf("Operating System : %s\n", runtime.GOOS)
-//	args := os.Args
+	//	args := os.Args
 	err := term.Init()
-	cbuf := ""
 	if err != nil {
 		panic(err)
 	}
@@ -21,46 +22,16 @@ func main() {
 	switch {
 	//-------------------------------------------------------------
 	case len(os.Args) == 2:
-	   fmt.Printf("cmd %s\n",os.Args[1])
-	   c1s.Cmd(os.Args[1])
-	fmt.Println("Not")
+		fmt.Printf("cmd %s\n", os.Args[1])
+		c1s.Run(os.Args[1])
+		fmt.Println("Not")
 
 		//}
 		//-------------------------------------------------------------
 	default:
-		cmd := ""
-		fmt.Printf("C1Script\n")
-		fmt.Printf("Operating System : %s\n", runtime.GOOS)
-		fmt.Println("Command Line Mode")
-		fmt.Printf(".")
-		for {
-			switch ev := term.PollEvent(); ev.Type {
-			case term.EventKey:
-				switch ev.Key {
-				case term.KeyArrowUp:
-					fmt.Println("Arrow Up pressed")
-				case term.KeyArrowDown:
-					fmt.Println("Arrow Down pressed")
-				case term.KeyEnter:
-					fmt.Printf("\n.")
-					cmd = cbuf
-					cbuf = ""
-				default:
-					s := rune(ev.Ch)
-					cbuf = cbuf + string(s)
-					fmt.Printf("%s", string(s))
-				}
-			}
-			switch {
-			case cmd == "exit":
-				fmt.Println("\nExit C1Script")
-				os.Exit(2)
-			case cmd == "?":
-				fmt.Println("\nHelp")
+		cmdctl.Command = "test"
+		cmdctl = c1s.Cmd(cmdctl)
+		fmt.Printf("Command %s\n", cmdctl.Command)
 
-				fmt.Printf("\n.")
-			}
-		}
 	}
-
 }
